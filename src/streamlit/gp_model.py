b65cb09a0f9a8e2c2595c42db5f1aa433133bd51
niklaskessel <nkessel@stud.fra-uas.de>
2025-07-02 13:54:16 +0200

    new models streamlit

---
+++ src/streamlit/gp_model.py
@@ -0,0 +1,18 @@
+import gpytorch
+import torch
+
+class DGPModel(gpytorch.models.ApproximateGP):
+    def __init__(self, input_dim):
+        inducing_points = torch.randn(128, input_dim)
+        variational_distribution = gpytorch.variational.MeanFieldVariationalDistribution(inducing_points.size(0))
+        variational_strategy = gpytorch.variational.VariationalStrategy(
+            self, inducing_points, variational_distribution, learn_inducing_locations=True
+        )
+        super().__init__(variational_strategy)
+        self.mean_module = gpytorch.means.ZeroMean()
+        self.covar_module = gpytorch.kernels.ScaleKernel(gpytorch.kernels.RBFKernel())
+
+    def forward(self, x):
+        mean = self.mean_module(x)
+        covar = self.covar_module(x)
+        return gpytorch.distributions.MultivariateNormal(mean, covar)
